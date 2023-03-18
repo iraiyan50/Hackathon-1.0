434d362a51126ae9661f1fb12020b56c7474dbc6
Hasib Altaf <hasibaltaf2839@gmail.com>
2023-03-18 14:51:07 +0600

    update 2

--- login_signup.cpp
+++ login_signup.cpp
@@ -1,13 +1,86 @@
 #pragma once
 
-#include<bits/stdc++.h>
-#include<iostream>
+#include <bits/stdc++.h>
+#include <iostream>
 
-void login(){
+void hidePass()
+{
+    char b;
+    int j;
+    while (1)
+    {
+        b = getch();
 
+        if (b == ENTER)
+        {
+            password[j] = NULL;
+            break;
+        }
+        else if (b == TAB)
+        {
+            continue;
+        }
+        else if (b == BCKSPC)
+        {
+            if (j > 0)
+            {
+                j--;
+                printf("\b \b");
+            }
+        }
+        else
+        {
+            password[j] = b;
+            j++;
+            printf("*");
+        }
+    }
 }
-void signup(){
-    
+
+void login()
+{
 }
-
-
+void signup()
+{
+    FILE *fp;
+    int c = 0;
+    char id[30], pass[30];
+    char username[30], password[30];
+    system("cls");
+    cout << "*********************************************************" << endl;
+    cout << "             Sales-Representative Signup Menu            " << endl;
+    cout << "*********************************************************" << endl;
+    cout << "-----------Please enter the Username & Password----------" << endl;
+    cout << endl;
+    cout << "\t1.Username: ";
+    cin >> username;
+    cout << "\t2.Password: ";
+    hidePass();
+    cin >> password;
+    // opening the file in read mode
+    char *user = "/Admin_Info/";
+    fp = fopen(username + ".txt", "r");
+    while ((fscanf(fp, "%s %s ", &id, &pass)) != EOF)
+    {
+        if (strcmp(username, id) == 0 && strcmp(password, pass) == 0)
+        // compares the info with database
+        {
+            cout << "\tCongratulations!! Your signup is complete.\n"
+                 << endl;
+            cout << "\tPlease login with your signup informations.\n"
+                 << endl;
+            c = 1;
+            break;
+        }
+    }
+    fclose(fp);
+    if (c != 1)
+    {
+        cout << "\tSignup Failed! Please try again" << endl;
+        signup(1);
+    }
+    else if (c == 1)
+    {
+        login(1);
+    }
+}
