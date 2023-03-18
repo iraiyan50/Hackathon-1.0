0a4a9d1f411ef3c10c2a2601faafaeef5bfac3b0
Hasib Altaf <hasibaltaf2839@gmail.com>
2023-03-18 22:15:41 +0600

    update 9

--- CV_edit.cpp
+++ CV_edit.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include"main.cpp"
 
 using namespace std;
 
@@ -44,30 +45,63 @@ void display(const CV& cv, ofstream& fout) {
         fout << "Duration: " << qualification.duration << endl << endl; // Add extra endl here
     }
 }
+struct Address{
+    char House_no[10];
+    char Road_no[10];
+    char Area[20];
+    char city[20];
+    char Postcode[15];
+    char country[20];
+};
+struct Personal_Info{
+    char name[30];
+    char email[30];
+    char phone[20];
+    char date_of_birth[15];
+    char marital_status[20];//if possible implement option system
+};
+void Personal_Info(){
+
+}
+
+void Professional_Info(){
+
+}
+void Educational_Info(){
+
+}
 void DataEntry(){
     
     int choice;
     cout<<"*********************************************************" << endl;
     cout << "              Digital CV Data Entry Menu                " << endl;
     cout << "*********************************************************" << endl;
-    cout << "\t1.Add info\n";
-    cout << "\t2.Logout\n";
+    cout << "\t1.Add Personal info\n";
+    cout << "\t2.Add Professional info\n";
+    cout << "\t3.Add Educational info\n";
+    cout << "\t4.Logout\n";
     cout << endl;
     cout << "Please Enter your choice: ";
     cin >> choice;
     switch(choice){
         case 1:
+            Personal_Info();
+            break;
+        case 2:
+            Professional_Info();
+            break;
+        case 3:
+            Educational_Info();
+            break;
+        case 4:
+            //main();
+            break;
         
     }
 
 
 }
-void Jobinput(){
 
-}
-void Eduinput(){
-
-}
 
 int main() {
     CV cv;
--- CV_edit.hpp
+++ CV_edit.hpp
@@ -6,24 +6,53 @@
 #include<fstream>
 using namespace std;
 
-const int MAX_JOBS = 10;
-const int MAX_QUALIFICATIONS = 10;
+#define MAX_JOBS 10
+#define MAX_QUALIFICATIONS 10
 
-struct Job {
-    string position;
-    string company;
-    string duration;
-    string description;
-};
-struct Qualification {
-    string degree;
-    string institution;
-    string duration;
-};
-struct CV {
-    string name;
-    string email;
-    string phone;//error to be given in phone number if not number
-    vector<Job> jobs;
-    vector<Qualification> qualifications;
-};
+typedef struct job
+{
+    char position[50];
+    char company[50];
+    char duration[20];
+    char description[500];
+}job;
+typedef struct qualification
+{
+    char degree[50];
+    char institution[50];
+    char duration[20];
+}qualification;
+typedef struct CV
+{
+    char name[50];
+    char email[50];
+    char phone[15];//48 - 0
+    struct job jobs[MAX_JOBS];
+    int num_jobs;
+    qualification qualifications[MAX_QUALIFICATIONS];
+    int num_qualifications;
+}CV;
+void display(struct CV cv, FILE *fp)
+{
+    fprintf(fp, "Name: %s\n", cv.name);
+    fprintf(fp, "Email: %s\n", cv.email);
+    fprintf(fp, "Phone: %s\n", cv.phone);
+    fprintf(fp, "\nWork Experience:\n");
+    for (int i = 0; i < cv.num_jobs; i++)
+    {
+        fprintf(fp, "Position: %s\n", cv.jobs[i].position);
+        fprintf(fp, "Company: %s\n", cv.jobs[i].company);
+        fprintf(fp, "Duration: %s\n", cv.jobs[i].duration);
+        fprintf(fp, "Description: %s\n", cv.jobs[i].description);
+    }
+    fprintf(fp, "\nEducation:\n");
+    for (int i = 0; i < cv.num_qualifications; i++)
+    {
+        fprintf(fp, "Degree: %s\n", cv.qualifications[i].degree);
+        fprintf(fp, "Institution: %s\n", cv.qualifications[i].institution);
+        fprintf(fp, "Duration: %s\n", cv.qualifications[i].duration);
+    }
+}
+void CVinput();
+void Jobinput();
+void Eduinput();
