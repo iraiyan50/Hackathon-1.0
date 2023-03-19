c9bbd55f70c353ee53305ddd019d1e752fb1a599
iraiyan50 <128201651+iraiyan50@users.noreply.github.com>
2023-03-19 07:18:54 +0600

    update CV file

--- CV_edit.cpp
+++ CV_edit.cpp
@@ -1,55 +1,15 @@
 #pragma once
+#include<bits/stdc++.h>
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <vector>
-//#include "main.cpp"
+// #include "main.cpp"
 #include "login_signup.cpp"
 
 using namespace std;
 
-const int MAX_JOBS = 10;
-const int MAX_QUALIFICATIONS = 10;
 
-struct Qualification
-{
-    string degree;
-    string institution;
-    string duration;
-};
-struct CV
-{
-    string name;
-    string email;
-    string phone; // error to be given in phone number if not number
-    vector<Job> jobs;
-    vector<Qualification> qualifications;
-};
-void display(const CV &cv, ofstream &fout)
-{
-    fout << "Name: " << cv.name << endl;
-    fout << "Email: " << cv.email << endl;
-    fout << "Phone: " << cv.phone << endl
-         << endl;
-    fout << "Work Experience:" << endl;
-    for (const auto &job : cv.jobs)
-    {
-        fout << "Position: " << job.position << endl;
-        fout << "Company: " << job.company << endl;
-        // fout << "Duration: " << job.duration << endl;
-        // fout << "Description: " << job.description << endl;
-    }
-    fout << endl
-         << "Education:" << endl;
-    for (const auto &qualification : cv.qualifications)
-    {
-        fout << "Degree: " << qualification.degree << endl;
-        fout << "Institution: " << qualification.institution << endl;
-        fout << "Duration: " << qualification.duration << endl
-             << endl; // Add extra endl here
-    }
-}
-struct Address
+// define the Address structure here
+/*struct Address
 {
     char House_no[10];
     char Road_no[10];
@@ -58,9 +18,12 @@ struct Address
     char Postcode[15];
     char country[20];
 };
+
+
 struct Skills_and_Achievments
 {
-    // pore
+    char skill[20];
+    char achievments[20];
 };
 struct Personal_Info
 {
@@ -73,25 +36,26 @@ struct Personal_Info
     char marital_status[20]; // if possible implement option system
     Address Present_Address;
     Address Permanent_Address;
-} Personal_Info;
+};
 struct Educational_Info
 {
     char Institution[30];
     char Passing_year[7];
     char Degree[10];
     char cgpa[6];
-} Educational_Info;
+};
 struct Professional_Info
 {
     char position[30];
     char company[30];
     char Start_date[15];
     char End_date[15];
-} Professional_Info;
-
+};
+*/
 void Personal_Info_scan()
 {
     FILE *fp;
+    struct Personal_Info personal1;
     char filename[30];
     strcpy(filename, name);
     strcat(filename, "_PI.txt");
@@ -101,55 +65,96 @@ void Personal_Info_scan()
     cout << "*********************************************************" << endl;
     cout << "-------------Please Enter the following informations-----" << endl;
     cout << endl;
-    cout << "\tName: ";
-    cin >> Personal_Info.Name;
-    cout << "\tEmail: ";
-    cin >> Personal_Info.email;
-    cout << "\tPhone No: ";
-    cin >> Personal_Info.phone;
-    cout << "\tGender: ";
-    cin >> Personal_Info.Gender;
-    cout << "\tNationality: ";
-    cin >> Personal_Info.Nationality;
-    cout << "\tDate of Birth: ";
-    cin >> Personal_Info.date_of_birth;
-    cout << "\tMarital Status: ";
-    cin >> Personal_Info.marital_status;
-    cout<<"\tPresent Address: \n";
-    cout<<"\t\tHouse no: ";
-    cin>>Personal_Info.Present_Address.House_no;
-    cout<<"\t\tRoad no: ";
-    cin>>Personal_Info.Present_Address.Road_no;
-    cout<<"\t\tArea: ";
-    cin>>Personal_Info.Present_Address.Area;
-    cout<<"\t\tCity: ";
-    cin>>Personal_Info.Present_Address.city;
-    cout<<"\t\tPostcode: ";
-    cin>>Personal_Info.Present_Address.Postcode;
-    cout<<"\t\tCountry: ";
-    cin>>Personal_Info.Present_Address.country;
 
-    //Permanent Address
-    cout<<"\tPermanent Address: \n";
-    cout<<"\t\tHouse no: ";
-    cin>>Personal_Info.Permanent_Address.House_no;
-    cout<<"\t\tRoad no: ";
-    cin>>Personal_Info.Permanent_Address.Road_no;
-    cout<<"\t\tArea: ";
-    cin>>Personal_Info.Permanent_Address.Area;
-    cout<<"\t\tCity: ";
-    cin>>Personal_Info.Permanent_Address.city;
-    cout<<"\t\tPostcode: ";
-    cin>>Personal_Info.Permanent_Address.Postcode;
-    cout<<"\t\tCountry: ";
-    cin>>Personal_Info.Permanent_Address.country;
-    
-    fprintf(fp, "");
+    cout << "\tName: ";
+    cin >> personal1.Name;
+    fprintf(fp, "Name: %s\n", personal1.Name);
+
+    cout << "\tEmail: ";
+    cin >> personal1.email;
+    fprintf(fp, "Email: %s\n", personal1.email);
+
+    cout << "\tPhone No: ";
+    cin >> personal1.phone;
+    fprintf(fp, "Phone No: %s\n", personal1.phone);
+
+    cout << "\tGender: ";
+    cin >> personal1.Gender;
+    fprintf(fp, "Gender: %s\n", personal1.Gender);
+
+    cout << "\tNationality: ";
+    cin >> personal1.Nationality;
+    fprintf(fp, "Nationality: %s\n", personal1.Nationality);
+
+    cout << "\tDate of Birth: ";
+    cin >> personal1.date_of_birth;
+    fprintf(fp, "Date of Birth: %s\n", personal1.date_of_birth);
+
+    cout << "\tMarital Status: ";
+    cin >> personal1.marital_status;
+    fprintf(fp, "Marital Status: %s\n", personal1.marital_status);
+
+    cout << "\tPresent Address: \n";
+    fprintf(fp, "Present Address: \n");
+
+    cout << "\t\tHouse no: ";
+    cin >> personal1.Present_Address.House_no;
+    fprintf(fp, "\tHouse no: %s\n", personal1.Present_Address.House_no);
+
+    cout << "\t\tRoad no: ";
+    cin >> personal1.Present_Address.Road_no;
+    fprintf(fp, "\tRoad no: %s\n", personal1.Present_Address.Road_no);
+
+    cout << "\t\tArea: ";
+    cin >> personal1.Present_Address.Area;
+    fprintf(fp, "\tArea: %s\n", personal1.Present_Address.Area);
+
+    cout << "\t\tCity: ";
+    cin >> personal1.Present_Address.city;
+    fprintf(fp, "\tCity: %s\n", personal1.Present_Address.city);
+
+    cout << "\t\tPostcode: ";
+    cin >> personal1.Present_Address.Postcode;
+    fprintf(fp, "\tPostcode: %s\n", personal1.Present_Address.Postcode);
+
+    cout << "\t\tCountry: ";
+    cin >> personal1.Present_Address.country;
+    fprintf(fp, "\tCountry: %s\n", personal1.Present_Address.country);
+
+    // Permanent Address
+    cout << "\tPermanent Address: \n";
+    fprintf(fp, "Present Address: \n");
+
+    cout << "\t\tHouse no: ";
+    cin >> personal1.Permanent_Address.House_no;
+    fprintf(fp, "\tHouse no: %s\n", personal1.Permanent_Address.House_no);
+
+    cout << "\t\tRoad no: ";
+    cin >> personal1.Permanent_Address.Road_no;
+    fprintf(fp, "\tRoad no: %s\n", personal1.Permanent_Address.Road_no);
+
+    cout << "\t\tArea: ";
+    cin >> personal1.Permanent_Address.Area;
+    fprintf(fp, "\tArea: %s\n", personal1.Permanent_Address.Area);
+
+    cout << "\t\tCity: ";
+    cin >> personal1.Permanent_Address.city;
+    fprintf(fp, "\tCity: %s\n", personal1.Permanent_Address.city);
+
+    cout << "\t\tPostcode: ";
+    cin >> personal1.Permanent_Address.Postcode;
+    fprintf(fp, "\tPostcode: %s\n", personal1.Permanent_Address.Postcode);
+
+    cout << "\t\tCountry: ";
+    cin >> personal1.Permanent_Address.country;
+    fprintf(fp, "\tCountry: %s\n", personal1.Permanent_Address.country);
+
+    fclose(fp);
 }
 void Professional_Info_Scan()
 {
-    
     FILE *fp;
+    struct Professional_Info profession1;
     char filename[30];
     strcpy(filename, name);
     strcat(filename, "_PfI.txt");
@@ -159,14 +164,56 @@ void Professional_Info_Scan()
     cout << "*********************************************************" << endl;
     cout << "-------------Please Enter the following informations-----" << endl;
     cout << endl;
-    cout<<"\tPosition: ";cin>>Professional_Info.position;
-    cout<<"\tCompany: ";cin>>Professional_Info.company;
-    cout<<"\tStart Date: ";cin>>Professional_Info.Start_date;
-    cout<<"\tEnd Date: ";cin>>Professional_Info.End_date;
-    fprintf
+
+    cout << "\tPosition: ";
+    cin >> profession1.position;
+    fprintf(fp, "Position: %s\n", profession1.position);
+
+    cout << "\tCompany: ";
+    cin >> profession1.company;
+    fprintf(fp, "Company: %s\n", profession1.company);
+
+    cout << "\tStart Date: ";
+    cin >> profession1.Start_date;
+    fprintf(fp, "Start: %s\n", profession1.Start_date);
+
+    cout << "\tEnd Date: ";
+    cin >> profession1.End_date;
+    fprintf(fp, "End Date: %s\n", profession1.End_date);
+
+    fclose(fp);
 }
-void Educational_Info()
+void Educational_Info_Scan()
 {
+    FILE *fp;
+    char filename[30];
+    struct Educational_Info edu1;
+    strcpy(filename, name);
+    strcat(filename, "_EI.txt");
+    fp = fopen(filename, "w");
+    cout << *"********************************************************" << endl;
+    cout << "                    Educational Info Menu                " << endl;
+    cout << "*********************************************************" << endl;
+    cout << "-------------Please Enter the following informations-----" << endl;
+    cout << endl;
+
+    cout << "\tInstitution: ";
+    cin >> edu1.Institution;
+    fprintf(fp, "Institution: %s\n", edu1.Institution);
+
+    cout << "\tPassing year: ";
+    cin >> edu1.Passing_year;
+    fprintf(fp, "Passing Year: %s\n", edu1.Passing_year);
+
+    cout << "\tDegree: ";
+    cin >> edu1.Degree;
+    fprintf(fp, "Degree: %s\n", edu1.Degree);
+
+    cout << "\tCGPA: ";
+    cin >> edu1.cgpa;
+    fprintf(fp, "CGPA: %s\n", edu1.cgpa);
+
+    fclose(fp);
 }
 void DataEntry()
 {
@@ -185,163 +232,16 @@ void DataEntry()
     switch (choice)
     {
     case 1:
-        Personal_Info();
+        Personal_Info_scan();
         break;
     case 2:
-        Professional_Info();
+        Professional_Info_Scan();
         break;
     case 3:
-        Educational_Info();
+        Educational_Info_Scan();
         break;
     case 4:
-        // main();
+        // start(); change
         break;
     }
 }
-
-/*int main()
-{
-    CV cv;
-    // cv inputs
-    cout << "Enter your name: ";
-    getline(cin, cv.name);
-
-    cout << "Enter your email: ";
-    getline(cin, cv.email);
-
-    cout << "Enter your phone number: ";
-    getline(cin, cv.phone);
-    int choice = 1;
-    while (choice != 0 && cv.jobs.size() < MAX_JOBS)
-    {
-        cout << "1. Add a new job" << endl;
-        cout << "0. Finish" << endl;
-        cout << "Enter your choice: ";
-        cin >> choice;
-        cin.ignore(); // fflush
-
-        if (choice == 0)
-        { // switch case diye korbo
-            break;
-        }
-        else if (choice != 1)
-        {
-            cout << "Invalid choice. Try again." << endl;
-            continue;
-        }
-        // job inputs
-        Job job;
-
-        cout << "Enter your job position: ";
-        getline(cin, job.position);
-
-        cout << "Enter the company name: ";
-        getline(cin, job.company);
-
-        cout << "Enter the duration of the job (e.g. May 2018 - Jul 2020): ";
-        // getline(cin, job.duration);
-
-        cout << "Enter a description of your role and responsibilities: ";
-        getline(cin, job.description);
-
-        cv.jobs.push_back(job);
-
-        cout << endl;
-    }
-    // qualifications
-    choice = 1;
-    while (choice != 0 && cv.qualifications.size() < MAX_QUALIFICATIONS)
-    {
-        cout << "1. Add a new qualification" << endl;
-        cout << "0. Finish" << endl;
-        cout << "Enter your choice: ";
-        cin >> choice;
-        cin.ignore();
-
-        if (choice == 0)
-        {
-            break;
-        }
-        else if (choice != 1)
-        {
-            cout << "Invalid choice. Try again." << endl;
-            continue;
-        }
-
-        Qualification qualification;
-
-        cout << "Enter your degree: ";
-        getline(cin, qualification.degree);
-
-        cout << "Enter the institution name: ";
-        getline(cin, qualification.institution);
-
-        cout << "Enter the duration of the degree (e.g. Sep 2014 - Jun 2018): ";
-        getline(cin, qualification.duration);
-
-        while (true)
-        {
-            cout << "Enter 1 to add work experience or 2 to add qualification (or 0 to finish): ";
-            cin >> choice;
-            cin.ignore(); // ignore the newline character left in the input stream
-
-            if (choice == 0)
-            {
-                break;
-            }
-            else if (choice == 1 && cv.jobs.size() < MAX_JOBS)
-            {
-                Job job;
-
-                cout << "Enter your job position: ";
-                getline(cin, job.position);
-
-                cout << "Enter the company name: ";
-                getline(cin, job.company);
-
-                cout << "Enter the duration of the job (e.g. May 2018 - Jul 2020): ";
-                getline(cin, job.duration);
-
-                cout << "Enter a description of your role and responsibilities: ";
-                getline(cin, job.description);
-
-                cv.jobs.push_back(job);
-            }
-            else if (choice == 2 && cv.qualifications.size() < MAX_QUALIFICATIONS)
-            {
-                Qualification qualification;
-
-                cout << "Enter your degree: ";
-                getline(cin, qualification.degree);
-
-                cout << "Enter the institution name: ";
-                getline(cin, qualification.institution);
-
-                cout << "Enter the duration of the degree (e.g. Sep 2014 - Jun 2018): ";
-                getline(cin, qualification.duration);
-
-                cv.qualifications.push_back(qualification);
-            }
-            else
-            {
-                cout << "Invalid choice or maximum number of items reached." << endl;
-            }
-        }
-
-        string filename;
-        cout << "Enter the output file name: ";
-        getline(cin, filename);
-
-        ofstream fout(filename);
-        if (!fout.is_open())
-        {
-            cerr << "Error: Unable to create file." << endl;
-            return 1;
-        }
-
-        display(cv, fout);
-        cout << "CV saved to " << filename << "." << endl;
-
-        return 0;
-    }
-}*/
